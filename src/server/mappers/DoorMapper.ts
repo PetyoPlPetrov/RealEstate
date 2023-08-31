e99100f5898c46c7a88bb62e3f838655a037bdcd
Petyo.Petrov <petyopetrov@Petyos-MacBook-Pro.local>
2023-09-01 00:00:44 +0300

    feat(usecase1): change dtos,mapper and use cases for apartment name prop.Add tests

--- src/server/mappers/DoorMapper.ts
+++ src/server/mappers/DoorMapper.ts
@@ -1,23 +1,35 @@
-import { Door } from '@/models/Door';
-import { injectable } from 'tsyringe';
-import { EntityMapper } from '@/server/lib/EntityMapper';
-import { DoorDto } from '@/__mocks__/dtos/DoorDto';
+import { ApartmentDto } from '@/__mocks__/dtos/ApartmentDto';
 import { BuildingDto } from '@/__mocks__/dtos/BuidlingDto';
+import { DoorDto } from '@/__mocks__/dtos/DoorDto';
+import { Door } from '@/models/Door';
+import { EntityMapper } from '@/server/lib/EntityMapper';
+import { injectable } from 'tsyringe';
 
 type BuildingDtosById = Record<string, BuildingDto>;
+type ApartDtosById = Record<string, ApartmentDto>;
 
 @injectable()
 export class DoorMapper implements EntityMapper<Door, DoorDto> {
-  public toDomain(doorDto: DoorDto, buildingDtosById: BuildingDtosById): Door {
+  public toDomain(
+    doorDto: DoorDto,
+    buildingDtosById: BuildingDtosById,
+    apartmentDtosById?: ApartDtosById,
+  ): Door {
     const buildingName = this.getBuildingName(
       buildingDtosById,
       doorDto.building_id,
+    );
+
+    const apartmentName = this.getApartmentName(
+      apartmentDtosById,
+      doorDto.apartment_id,
     );
 
     return {
       id: doorDto.id,
       name: doorDto.name,
       buildingName,
+      apartmentName,
       connectionType: doorDto.connection_type,
       connectionStatus: doorDto.connection_status,
       lastConnectionStatusUpdate: doorDto.last_connection_status_update,
@@ -28,5 +40,18 @@ export class DoorMapper implements EntityMapper<Door, DoorDto> {
     const building = buildingDtos[id];
 
     return building ? `${building.street} ${building.street_no}` : 'n/a';
+  }
+
+  private getApartmentName(
+    apartDtos: ApartDtosById | undefined,
+    id: string | undefined,
+  ) {
+    if (apartDtos == undefined || !id) {
+      return 'n/a';
+    }
+
+    const apart = apartDtos[id];
+
+    return apart ? `${apart.name}` : 'n/a';
   }
 }
--- src/server/useCases/GetDoorByIdUseCase.ts
+++ src/server/useCases/GetDoorByIdUseCase.ts
@@ -1,10 +1,11 @@
-import { injectable } from 'tsyringe';
-import createHttpError from 'http-errors';
 import { Door } from '@/models/Door';
 import { UseCase } from '@/server/lib/UseCase';
-import { DoorRepository } from '@/server/repositories/DoorRepository';
-import { BuildingRepository } from '@/server/repositories/BuildingRepository';
 import { DoorMapper } from '@/server/mappers/DoorMapper';
+import { ApartmentRepository } from '@/server/repositories/ApartmentRepository';
+import { BuildingRepository } from '@/server/repositories/BuildingRepository';
+import { DoorRepository } from '@/server/repositories/DoorRepository';
+import createHttpError from 'http-errors';
+import { injectable } from 'tsyringe';
 
 interface Context {
   doorId: string;
@@ -16,10 +17,12 @@ export class GetDoorByIdUseCase implements UseCase<Door, Context> {
     private doorRepository: DoorRepository,
     private buildingRepository: BuildingRepository,
     private doorMapper: DoorMapper,
+    private apartmentRepository: ApartmentRepository,
   ) {}
 
   public async execute({ doorId }: Context) {
     const doorDto = await this.doorRepository.getDoorById(doorId);
+    let apartmentDto;
 
     if (!doorDto) {
       throw new createHttpError.NotFound(`no door found for id ${doorId}`);
@@ -35,8 +38,26 @@ export class GetDoorByIdUseCase implements UseCase<Door, Context> {
       );
     }
 
-    return this.doorMapper.toDomain(doorDto, {
+    if (doorDto.apartment_id) {
+      apartmentDto = await this.apartmentRepository.getApartmentById(
+        doorDto.apartment_id,
+      );
+    }
+
+    const apartmentDtosById = apartmentDto
+      ? {
+          [apartmentDto.id]: apartmentDto,
+        }
+      : undefined;
+
+    const buildingDtosById = {
       [buildingDto.id]: buildingDto,
-    });
+    };
+
+    return this.doorMapper.toDomain(
+      doorDto,
+      buildingDtosById,
+      apartmentDtosById,
+    );
   }
 }
